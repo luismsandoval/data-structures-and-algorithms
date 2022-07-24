a59595319ba87685982bcfac9fd880935f026c1e
Luis Sandoval <luismsandoval96@gmail.com>
2022-07-23 18:10:00 -0700

    completed node list

--- javascript/code401/linked-list/src/LinkedList.test.ts
+++ javascript/code401/linked-list/src/LinkedList.test.ts
@@ -6,4 +6,18 @@ describe("linked list", () => {
     const list: Collection<string> = new LinkedList<string>();
     expect(list).toBeDefined();
   });
+
+  it("instantiates an empty list", () => {});
+
+  it("inserts into linked list", () => {});
+
+  it("head points to first node", () => {});
+
+  it("inserts multiple nodes into linked list", () => {});
+
+  it("", () => {})
+
+  it("", () => {})
+  
+  it("", () => {})
 });
